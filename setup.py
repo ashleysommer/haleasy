2433f651654b0cb7b3588efbf26efd28215174b1
Matt Clark <matt@mattclark.net>
2015-02-09 16:12:32 +0000

    Removed test files from PyPi source dist

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 from setuptools import setup
 setup(
     name = "HALEasy",
-    version = "0.2.1",
+    version = "0.2.2",
     py_modules = ['haleasy'],
 
     # metadata for upload to PyPI
