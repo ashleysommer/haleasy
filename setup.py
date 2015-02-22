33fd670e76f0cf25cfe5dcb479ca623320e254df
Matt Clark <matt@mattclark.net>
2015-02-22 10:01:51 +0000

    Incremented PyPi minor minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 from setuptools import setup
 setup(
     name = "HALEasy",
-    version = "0.3.0",
+    version = "0.3.1",
     py_modules = ['haleasy'],
 
     # metadata for upload to PyPI
