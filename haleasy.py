9ce735c7416d8a9905cae48b00fe379dba2db6cd
Matt Clark <matt@mattclark.net>
2015-06-19 17:13:16 +0100

    Use a dedicated class for making lists of HALEasyLink objects

--- haleasy.py
+++ haleasy.py
@@ -152,31 +152,30 @@ class HALEasyLink(dougrain.link.Link):
         return str(self.as_object_with_rel())
 
 
-class HALEasy(object):
-    HTTP_CLIENT_CLASS = HALHttpClient
-    LINK_CLASS = HALEasyLink
+class HALDocLinkList(list):
+    def __init__(self, doc, host, link_class, haleasy_class):
+        super(HALDocLinkList, self).__init__()
 
-    def _add_links(self):
-        self._link_list = []
-        for rel, links in six.iteritems(self.doc.links):
+        # Add all the links from the _links sections
+        for rel, links in six.iteritems(doc.links):
             for link in listify(links):
-                self._link_list.append(self.LINK_CLASS(link.as_object(),
-                                                       base_uri=self.host,
-                                                       rel=rel,
-                                                       hal_class=type(self)))
+                self.append(link_class(link.as_object(),
+                                       base_uri=host,
+                                       rel=rel,
+                                       hal_class=haleasy_class))
 
-    def _add_embedded_as_links(self):
-        for rel in self.doc.embedded:
-            for embedded_resource in listify(self.doc.embedded[rel]):
+        # Add (or enhance) links to embedded resources
+        for rel in doc.embedded:
+            for embedded_resource in listify(doc.embedded[rel]):
                 # create a HALEasy object for each embedded resource
-                preview = type(self)(make_preview_url(embedded_resource.url(), self.host),
-                                     json_str=json.dumps(embedded_resource.as_object()),
-                                     is_preview=True)
+                preview = haleasy_class(make_preview_url(embedded_resource.url(), host),
+                                        json_str=json.dumps(embedded_resource.as_object()),
+                                        is_preview=True)
                 try:
                     # if there are links to the embedded resource in the parent document, set the .preview attribute
                     # of those links to the embedded resource
                     direct_links = []
-                    for link in self.links(rel=rel, href=preview.doc.links['self'].href):
+                    for link in self.links(doc.expand_curie, rel=rel, href=preview.doc.links['self'].href):
                         link.preview = preview
                         direct_links.append(link)
                     if not direct_links:
@@ -190,12 +189,59 @@ class HALEasy(object):
                         self_link_properties = preview.link(rel='self').as_object()
                     except LinkNotFoundError:
                         self_link_properties = {'href': ''}
-                    new_link = self.LINK_CLASS(self_link_properties,
-                                               base_uri=preview.host,
-                                               rel=rel,
-                                               hal_class=type(self),
-                                               preview=preview)
-                    self._link_list.append(new_link)
+                    new_link = link_class(self_link_properties,
+                                          base_uri=preview.host,
+                                          rel=rel,
+                                          hal_class=haleasy_class,
+                                          preview=preview)
+                    self.append(new_link)
+
+    def links(self, __curie_expander, **want_params):
+        """
+        Return an iterator over the links that match the given names and values in the want_params dict.  To get all
+        links pass nothing - the links you get will have their 'rel' attribute populated.  No exception will be raised
+        if the list of links returned is empty, as typical usage is expected to be for l in h.links(rel='somerel'):
+        and the loop body will simply not be executed
+
+        H.links()
+        H.links(rel='next')
+        H.links(rel='next', profile='video')
+        """
+        if 'rel' in want_params:
+            want_params['rel'] = __curie_expander(want_params['rel'])
+        links_found = []
+        for link in self:
+            if not want_params:
+                links_found.append(link)
+            else:
+                has_params = link.as_object_with_rel()
+                for k, v in six.iteritems(want_params):
+                    try:
+                        if has_params[k] != v:
+                            break  # the key exists but the values don't match
+                    except KeyError:
+                        break  # the key doesn't exist
+                else:  # this else belongs to the for loop - executed if all param values matched
+                    links_found.append(link)
+        return links_found
+
+    def link(self, __curie_expander, **want_params):
+        """
+        Return only the first link matching the want_params dict.  Use this if you are confident there is only one link
+        for a given match, which is quite common for singular rels.  It will raise a LinkNotFoundError if no matching
+        link is found, to help avoid subtle bugs if the returned value isn't used immediately
+        """
+        try:
+            return self.links(__curie_expander, **want_params)[0]
+        except IndexError:
+            raise LinkNotFoundError('no link matching %s found, document is %s' % (want_params, self))
+
+
+
+
+class HALEasy(object):
+    HTTP_CLIENT_CLASS = HALHttpClient
+    LINK_CLASS = HALEasyLink
 
     def __init__(self,
                  url,
@@ -209,6 +255,10 @@ class HALEasy(object):
         # If json_str is provided then we use that to build the document, otherwise we follow the url.  Note even when
         # providing a json_str you also need to provide a URL, because this is a HAL client, not a HAL document parser,
         # and without a URL it can't always know where to go next
+        self.fetched_from = None
+        self.doc = None
+        self._link_list = None
+        self.is_preview = is_preview
         self._maybe_set_http_client_class(http_client_class)
         if not json_str:
             self.from_url(url, method=method, data=data, **kwargs)
@@ -234,8 +284,7 @@ class HALEasy(object):
         self.fetched_from = url
         self.doc = dougrain.Document.from_object(json.loads(json_str), base_uri=url)
         self.is_preview = is_preview
-        self._add_links()
-        self._add_embedded_as_links()
+        self._link_list = HALDocLinkList(self.doc, self.host, self.LINK_CLASS, type(self))
 
     @property
     def host(self):
@@ -269,44 +318,10 @@ class HALEasy(object):
         return self.doc.properties
 
     def links(self, **want_params):
-        """
-        Return an iterator over the links that match the given names and values in the want_params dict.  To get all
-        links pass nothing - the links you get will have their 'rel' attribute populated.  No exception will be raised
-        if the list of links returned is empty, as typical usage is expected to be for l in h.links(rel='somerel'):
-        and the loop body will simply not be executed
-
-        H.links()
-        H.links(rel='next')
-        H.links(rel='next', profile='video')
-        """
-        if 'rel' in want_params:
-            want_params['rel'] = self.doc.expand_curie(want_params['rel'])
-        links_found = []
-        for link in self._link_list:
-            if not want_params:
-                links_found.append(link)
-            else:
-                has_params = link.as_object_with_rel()
-                for k, v in six.iteritems(want_params):
-                    try:
-                        if has_params[k] != v:
-                            break  # the key exists but the values don't match
-                    except KeyError:
-                        break  # the key doesn't exist
-                else:  # this else belongs to the for loop - executed if all param values matched
-                    links_found.append(link)
-        return links_found
+        return self._link_list.links(self.doc.expand_curie, **want_params)
 
     def link(self, **want_params):
-        """
-        Return only the first link matching the want_params dict.  Use this if you are confident there is only one link
-        for a given match, which is quite common for singular rels.  It will raise a LinkNotFoundError if no matching
-        link is found, to help avoid subtle bugs if the returned value isn't used immediately
-        """
-        try:
-            return self.links(**want_params)[0]
-        except IndexError:
-            raise LinkNotFoundError('no link matching %s found, document is %s' % (want_params, self))
+        return self._link_list.link(self.doc.expand_curie, **want_params)
 
     def rels(self):
         return self.doc.links.keys()
